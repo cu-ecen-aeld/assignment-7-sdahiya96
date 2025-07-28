2557cee8342e13fa23df7f270392242504448c3c
Siddharth Dahiya <sid.86dahiya@gmail.com>
2025-07-28 23:05:03 +0530

    Fixed module load error insmod to modprobe

--- misc-modules/hello.c
+++ misc-modules/hello.c
@@ -7,13 +7,13 @@ MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	printk(KERN_ALERT "Hello, world (build by sdahiya96)\n");
 	return 0;
 }
 
 static void hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_ALERT "Goodbye, cruel world (built by sdahiya96)\n");
 }
 
 module_init(hello_init);
